5a18c3332e110f526dfe0947f6e1a0a7a6dce74f
j-newberry <new17004@byui.edu>
2020-01-14 13:42:32 -0700

    changes

--- scripts/currentDate.js
+++ scripts/currentDate.js
@@ -4,4 +4,8 @@ console.log(currentDate);
 var currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
+var lastModified = document.lastModified;
+
 document.getElementById("currentYear").innerHTML = currentYear;
+
+document.getElementById("lastModified").innerHTML = lastModified;
