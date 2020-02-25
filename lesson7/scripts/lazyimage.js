d088f0e9210dc0471868c4728609a5f0091e0714
j-newberry <new17004@byui.edu>
2020-02-25 10:58:07 -0700

    table build

--- lesson7/scripts/lazyimage.js
+++ lesson7/scripts/lazyimage.js
@@ -1,4 +1,5 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
@@ -6,15 +7,22 @@ const loadImages = (image) => {
   };
 };
 
-  if('IntersectionObserver' in window) {
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
+};
+
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
+
       });
-    });
+    }, imgOptions);
+
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
---
+++ lesson8/ scripts/main.js
@@ -0,0 +1,84 @@
+/*Wednesday, 20 May 2020 */
+/* declare date variable*/
+let currentDate = new Date();
+let fullDate;
+
+/*day of week */
+let daysOfWeek = [ 
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+let dayOfWeek = currentDate.getDay();
+fullDate = daysOfWeek[dayOfWeek];
+
+/* day of month*/
+
+let dayOfMonth = currentDate.getDate();
+fullDate += ', ' + dayOfMonth;
+
+/*month*/
+
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'Decemeber'
+];
+fullDate += ' ' +  months[currentDate.getMonth()];
+
+/*year*/
+let year = currentDate.getFullYear();
+fullDate += ' ' + year;
+
+/* full date */ 
+document.querySelector('#current-date').textContent = fullDate;
+
+
+
+
+
+
+/*weather summary*/
+
+let f, t, s;
+t = 50;
+s = 10;
+
+if(t <= 50 && s >= 3) {
+    f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    document.querySelector("#windchill").innerHTML = f.toFixed(2) + '&deg';
+
+}
+else {
+    f = 'N/A';
+    document.querySelector("#windchill").innerHTML = f;
+
+}
+document.querySelector("#currenttemp").innerHTML =t + '&deg;'; /*'50&deg'*/;
+document.querySelector("#windspeed").innerHTML = s + 'mph';/*'10mph'*/;
+
+
+/* pancake */
+//const currentDate =  new Date();
+const aside = document.querySelector('aside');
+
+if (currentDate.getDay() === 5) {
+    aside.style.display = 'block';
+}
+    else {
+        aside.style.display = 'none';
+    }
